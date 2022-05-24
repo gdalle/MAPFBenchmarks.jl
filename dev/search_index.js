3646e945901f29413dfee5650592b9350e85f1dc
Documenter.jl <documenter@juliadocs.github.io>
2022-05-24 10:01:24 +0000

    build based on 83e1a82

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MAPFBenchmarks","category":"page"},{"location":"#MAPFBenchmarks","page":"Home","title":"MAPFBenchmarks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MAPFBenchmarks.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MAPFBenchmarks]","category":"page"}]
+}
